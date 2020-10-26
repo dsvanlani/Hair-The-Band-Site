e3819f331c206998ce9ac5f06968dc28d29c8c44
David Vanlaningham <dsvanlani@gmail.com>
2020-10-26 15:42:10 -0400

    Second Draft

--- Hair V2/javascript/index.js
+++ Hair V2/javascript/index.js
@@ -1,5 +1,23 @@
-window.addEventListener("scroll", () =>
-    if (window.scrollY == 100) {
-      document.querySelector(nav).classList.remove('fixed-top')
-    }
-)
+function changeView() {
+  data = this.dataset
+
+  // set all views display to none
+  document.querySelectorAll('.view').forEach(view => {
+    view.style.display = "none"
+  });
+
+  //set target display to block
+  document.querySelector(data.targetview).style.display = "block"
+
+  // scroll to page location if it exists
+  distance = document.querySelector(data.pagelocation).scrollTop
+  console.log(distance)
+  console.log(document.querySelector(data.pagelocation))
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.change-view-link').forEach(element => {
+    element.onclick = changeView
+  });
+
+})
